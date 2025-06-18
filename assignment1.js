f22bc332d0c32d0cceec98255b0a0f062e529d7e
Gjizzy <yaquelin.lopez@sdgku.edu>
2025-06-18 16:12:50 -0700

    assignment 1

---
+++ assignment1.js
@@ -0,0 +1,23 @@
+console.log("A funny story");
+
+let student = "Yaquelin";
+let favoriteFood = "Tacos";
+let place = "Taco Restaurant";
+let action = "the music is on";
+let time = "in the late afternoon";
+console.log("Hi my name is" + student);
+
+
+const occasion = "Happy Tuesday!";
+
+
+console.log(student + " favorite's food" + favoriteFood + " at the best at" + place + " while " + action);
+console.log("It was" + occasion + " and there was alot of people and the food was delicious");
+
+student = "Rebecca";
+favoriteFood = "Pizza";
+place = "at Dominoes"
+action = "while playing ";
+
+console.log(student + " favorite's food" + favoriteFood + " at the best at" + place + " while " + action);
+console.log("It was" + occasion + " and there was alot of people and the food was delicious");
---
+++ script.js
@@ -0,0 +1,59 @@
+console.log("Script");
+//VARIABLES
+
+//Strings
+let firstName = "Yaquelin";  // assing the value
+let lastName = "Lopez"; 
+
+//numerical
+let age = 99;
+let mySize = 10;
+
+//boolean values
+let isStudent = false;
+
+console.log;(firstName);
+console.log(age,mySize);
+
+//concatenation
+console.log("My name is " + firstName + " " + lastName);
+
+//Operations
+
+let num1 = 10;
+let num2 = 2;
+
+let addition = num1 + num2;
+// subs,div, mult
+
+
+console.log("Addition is: " + addition);
+
+
+console.log("Add is: " +add);
+console.log("Subs is: " +subs);
+console.log("Div is: " +div);
+console.log("Mult is: " +mult);
+
+let a;
+const pi = 3.1416; // constant value
+let r= 3;
+
+a = pi * (r*r);
+console.log("The area of a circle with a radius of " + r + " is the " + a. toFixed(2)};
+l
+let aVar = 20;
+letotherVar = 30
+
+console.log(aVar,otherVar);
+
+aVar = 50;
+OtherVar = 60;
+
+console.log(aVar,otherVar);
+
+const weekDays = 5;
+
+//weekDays = 8; // the constant can't change
+
+console.log(weekDays);
