8c0ab8cadc0f0a4e07e30b19eca00fa41b781b47
Gjizzy <yaquelin.lopez@sdgku.edu>
2025-06-23 16:50:39 -0700

    if-statements

---
+++ game.js
@@ -0,0 +1,9 @@
+let positionleft = 350;
+let character = document
+
+
+positionLeft = positionLeft-5;
+}
+console.log{positionLeft);
+    console.log(positionsLeft);
+console.log(positionsLeft);
---
+++ if-statements.js
@@ -0,0 +1,158 @@
+console.log("Conditionals");
+
+//if-statement (condition =
+//if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}else{}
+//code to run if the condition is false
+//}
+
+let isTrue = false;
+
+if(isTrue == true){
+     console.log("Yes");
+     }else{
+        console.log("No");
+        }
+
+        let waterTemp = 102;
+
+        if(waterTemp >= 100){
+            console.log("The water is boiling");
+        }else{
+            console.log("The water is NOT boiling");
+        }
+
+        //Challenge 1:
+        // check if the age is greater than 21
+        // T:display "you are an adult"
+        // F: display "you are underage"
+
+let age = 30;
+
+if(age>21){
+    console.log("you are an adult");
+    }else{
+        console.log("you are underage");
+    }
+
+    // else-if statement
+    //if(condition){
+    // code to be executed if the condition is true
+    //}else if (condition2) {
+    // code to runif the conditio2n is true
+    //}else{
+    // code to run if conditions were false
+    //}
+
+    age = prompt("Enter your age:");
+
+    if(age<13){
+        console.log("You are a child");
+    }else if (age<21){
+        console.log("You are a teenager");
+    }else if(age<64){
+        console.log("You are a an adult");
+    }else{
+        console.log("You are a senior");
+    }
+
+
+    //chalenge 2:
+
+    // Scenario:
+    // You're designing a sysstem of self driving bikes.
+    // Instructions:
+    // Ask for the traffic light color
+    // ("green", "yelloe", or"red"),and tell the
+    // bike what to do (Go!, Slow down, stop)
+    
+
+
+
+     console.log()
+}
+   console.log(Slow down");
+}else  if (light=="red"){
+console.log("Stop");
+}else{
+    console.log("Invalid color!")
+  // }
+
+  function ticketCalculator(){
+    // the code of your conditions should be here 
+    let age = prompt ("Enter your age");
+    let price =0;
+
+    if(age<12){
+         price=5
+    }else if(age<18){
+        price=8;
+    }else{
+        price=10;
+    }
+
+    return"Your ticket price is  " + price;
+  }
+
+document.write(ticketCalculato());
+
+function weather Outfit(){
+let tmp = prompt("Enter ")
+let outfit="";
+let p = document.getElementById("outfit");
+
+if(tmp<15){
+    outfit="Jacket";
+}else if(tmp<25){}
+    outfit="swaeter";
+}else{}
+outfit="t-shirt";
+}
+
+package.innerHTML="You should wear: " + outfit;
+}
+    
+    //Switch-case
+    //use for checking one single
+
+
+
+switch
+
+    console
+    case"run":
+
+    case"attack"
+    console.log (You are attacking a enemy");
+
+
+        default:
+            console.log("Unknown action, Try again");
+    }
+       break;
+    deafualt:
+       console.log("Unknown action. Try gain");
+}
+  }
+  // Challenge
+  // Instructions:
+  // Use the switch example provided.
+  // Add two more functions that a game character can do.
+
